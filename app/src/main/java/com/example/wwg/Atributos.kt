1d74652d8fa8a103322d27f07846bb0d81333ddd
Manuel Palacios <manuelph1984@gmail.com>
2021-11-30 00:08:39 +0100

    Desing Atribbutos

---
+++ app/src/main/java/com/example/wwg/Atributos.kt
@@ -0,0 +1,11 @@
+package com.example.wwg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Atributos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atributos)
+    }
+}
