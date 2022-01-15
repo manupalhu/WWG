ee0caf0b1baccf7ff19b8bd689da40fced0e3c4a
Manuel Palacios <manuelph1984@gmail.com>
2022-01-15 22:49:47 +0100

    Desing Atribbutos

--- app/src/main/java/com/example/wwg/Stats.kt
+++ app/src/main/java/com/example/wwg/Stats.kt
@@ -3,9 +3,9 @@ package com.example.wwg
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Atributos : AppCompatActivity() {
+class Stats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_atributos)
+        setContentView(R.layout.activity_stats)
     }
 }
